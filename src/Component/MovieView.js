9f54a5e82df86e06bc5a1bdeeabd920de7be247f
Your Name <adeyeye17@gmail.com>
2021-10-17 12:20:13 +0200

    Loading state was implemented

--- src/Component/MovieView.js
+++ src/Component/MovieView.js
@@ -5,21 +5,32 @@ import {useState, useEffect} from "react"
 
 export default function MovieView() {
     const {id} = useParams()
- 
+    const [isLoading,setIsLoading] = useState(true)
     
     const [movieDetails, setMovieDetails] = useState({})
     useEffect(()=>{
           fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=0359fd758c84889f671ad1bcaab6eada&language=en-US`)
                 .then(response => response.json())
                 .then(data =>{
-                    setMovieDetails(data)
-
+                        setMovieDetails(data)
+                        setIsLoading(false)
                 })
     }, [id])
-  return (
-    <div>
-      <Hero text={movieDetails.original_title} />
-    
-    </div>
-  );
+
+
+    function renderMovieDetails(){
+        if(isLoading){
+            return <Hero text="Loading..."/>
+        } 
+        if(movieDetails){
+              return <Hero text={movieDetails.original_title} />
+        }
+
+    }
+  return(
+<div>
+  {  renderMovieDetails()}
+</div>
+  ) 
+  
 }
